17d8e4193442b69d2e24c729d208ad8afb019ccf
codezzmaven <coezzmaven@gmail.com>
2017-01-29 01:04:09 +0100

    Added the button to change map colour.

--- leaflet-india-states/india-states/india.js
+++ leaflet-india-states/india-states/india.js
@@ -1,10 +1,25 @@
 window.onload = function() {
+  function getColor(d) {
+  return d == "Union Territor" ? '#FED976' :
+         d == "State"  ? '#FFEDA0' :
 
+                    '#FED976';
+}
+function style2(feature) {
+    return {
+        fillColor: getColor(feature.properties.TYPE_1),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
     //var map = L.map('map').setView([-41.2858, 174.78682], 14);
     var map = L.map('map').setView([21.48885, 76.99219], 5.3);
-    
-    
-    mapLink = 
+
+
+    mapLink =
         '<a href="http://openstreetmap.org">OpenStreetMap</a>';
     L.tileLayer(
         'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,8 +27,8 @@ window.onload = function() {
             id: 'map.light',
         maxZoom: 18,
         }).addTo(map);
-    
- /*   
+
+ /*
     var popup = L.popup();
 
     function onMapClick(e) {
@@ -24,16 +39,23 @@ window.onload = function() {
     }
 
     map.on('click', onMapClick);
-    
-    
+
+
     */
-    
 
-    L.geoJson(indian_states).addTo(map);
+    map.on('click', function(e) {
+        alert(e.latlng); // e is an event object (MouseEvent in this case)
+    });
+    var n = L.geoJson(indian_states).addTo(map);
+    //L.geoJson(indian_states, {style: style2}).addTo(map);
+    $('#mybutton').on('click', function () {
+      console.log("helloss")
+      n.setStyle(style2);
 
-    
-    
+    })
+
+
     console.log('It got executed')
-    
-    
+
+
 }
